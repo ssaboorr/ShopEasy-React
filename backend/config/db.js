db9ff345bb87c4cf684f3f93b52d298d86a1e51e
ssaboorr <48275468+ssaboorr@users.noreply.github.com>
2022-01-24 05:46:25 -0500

    Step 12: Added Backend and connected to Mongo DB

---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+
+const connectDb = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI,{
+            useUnifiedTopology:true,
+            useNewUrlParser:true,
+        });
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`)
+        
+    } catch (err) {
+        console.error(`Error: ${err.message}`);
+        process.exit(1)
+        
+    }
+
+}
+
+export default connectDb;
--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,13 @@ import products from'./data/products.js'
 import fashion from './data/fashion.js';
 import phone from './data/phone.js'
 import footwear from './data/shoes.js'
-
+import connectDb from './config/db.js';
 import dotenv from 'dotenv'
 import shoes from './data/shoes.js';
 
 dotenv.config();
+connectDb();
+
 const app = express();
 
 // let fashion = products[0]
@@ -68,5 +70,5 @@ app.get('/api/products/phone/:id',(req,res) => {
 
 
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
 app.listen(PORT,() => console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
