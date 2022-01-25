2f538980e76d63d8a95189d06b61de8d06086fd6
ssaboorr <48275468+ssaboorr@users.noreply.github.com>
2022-01-25 09:13:28 -0500

    step 20:  Made custom Middleware (error Handler)

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
 import Products from "../models/productModel.js";
 
 const router = express.Router();
@@ -9,7 +10,6 @@ router.get(
 
   asyncHandler(async (req, res) => {
     let products = await Products.find({});
-
     res.json(products);
   })
 );
@@ -17,46 +17,14 @@ router.get(
 router.get(
   "/:id",
   asyncHandler(async (req, res) => {
-    try {
-      const product = await Products.findById(req.params.id);
+    const product = await Product.findById(req.params.id);
 
-      if (product) {
-        res.json(product);
-      } else {
-        res.status(404).json({ message: "Product Not Found" });
-      }
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: "Errrroooooeeeeeeerrrrr" });
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "Product Not FOund" });
     }
   })
 );
-
-// router.get(
-//   "/product/:id",
-//   asyncHandler(async (req, res) => {
-//     try {
-//       const product = await Products.findById(req.params.id);
-
-//       if (product) {
-//         res.json(product);
-//       } else {
-//         res.status(404).json({ message: "Product Not Found" });
-//       }
-//     } catch (err) {
-//       console.error(err);
-//       res.status(500).json({ message: "Errrroooooeeeeeeerrrrr" });
-//     }
-//   })
-// );
-
-// router.get(
-//   "/fashion",
-//   asyncHandler(async (req, res) => {
-//     let products = await Products.find(prod => prod.type === 'fashion');
-//     // products = products.filter((prod) => prod.type === "fashion");
-//     res.json(products);
-//   })
-// );
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,75 +1,28 @@
-import express from 'express'
-import products from'./data/products.js'
-import fashion from './data/fashion.js';
-import phone from './data/phone.js'
-import footwear from './data/shoes.js'
-import connectDb from './config/db.js';
-import dotenv from 'dotenv'
-import shoes from './data/shoes.js';
-import colors from 'colors';
-import productRoutes from './routes/productRoutes.js'
-
+import express from "express";
+import connectDb from "./config/db.js";
+import dotenv from "dotenv";
+import colors from "colors";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middlewares/errorMiddlewares.js";
 
 dotenv.config();
 connectDb();
 
 const app = express();
 
-app.get('/',(req,res)=> {
-    res.send("api is running")
-})
+app.get("/", (req, res) => {
+  res.send("api is running");
+});
 
+app.use("/api/product", productRoutes);
 
-// app.get('/fashion',productRoutes)
-
-
-app.use('/api/product',productRoutes)
-
-
-// app.get('/api/products/fashion',(req,res)=> {
-//     res.json(fashion)
-// })
-
-// app.get('/api/products/footwear',(req,res) => {
-//     res.json(footwear)
-// })
-
-// app.get('/api/products/phone',(req,res)=>{
-//     res.json(phone)
-// })
-
-
-// app.get('/api/products/:id',(req,res)=>{
-//     const product = products.find((product) => product._id === +req.params.id)
-//     res.json(product)
-// })
-
-app.get('/api/products/footwear/:id',(req,res)=> {
-    const shoe = shoes.find((shoe) => shoe._id === +req.params.id)
-    res.json(shoe)
-})
-
-app.get('/api/products/fashion/:id',(req,res) => {
-    const cloth = fashion.find((cloth) => cloth._id === +req.params.id) 
-    res.json(cloth)
-})
-
-app.get('/api/products/phone/:id',(req,res) => {
-    const phones = phone.find((phone) => phone._id === +req.params.id )
-    res.json(phones)
-})
-
-
-// app.get('/api/footwear',(req,res) => {
-//     res.json(shoes)
-// })
-
-// app.get('/api/phone',(req,res)=>{
-//     res.json(phone)
-// })
-
-
-
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5001;
-app.listen(PORT,() => console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+app.listen(PORT, () =>
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
+  )
+);
