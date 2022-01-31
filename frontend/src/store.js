7a3c1410e649205c7c8ebecc062a8c53cad8b0cd
ssaboorr <48275468+ssaboorr@users.noreply.github.com>
2022-01-31 13:49:45 -0500

    Step 22 completed. Created  first reducer done

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { productListReducer } from "./reducers/prodcuctReducer";
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  productList: productListReducer,
+});
 
 const initialState = {};
 
@@ -13,6 +16,5 @@ const store = createStore(
   initialState,
   composeWithDevTools(applyMiddleware(...middlewares))
 );
-
 
 export default store;
