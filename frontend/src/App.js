c81d08bac95bbf6128df2d65b32496528d20c26a
ssaboorr <48275468+ssaboorr@users.noreply.github.com>
2022-02-14 14:20:02 -0500

    Step 46 completed. Place Order Screen done.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen"
+import PlaceOrderScreen from "./screens/PlaceorderScreen";
 
 
 const App = () => {
@@ -54,6 +55,8 @@ const App = () => {
           <Route path="/profile" element={<ProfileScreen />} />
           <Route path="/shipping" element={<ShippingScreen />} />
           <Route path="/payments" element={<PaymentScreen />} />
+
+          <Route path="/placeorder" element={<PlaceOrderScreen />} />
 
 
 
---
+++ frontend/src/screens/PlaceorderScreen.js
@@ -0,0 +1,195 @@
+import { Link as RouterLink } from "react-router-dom";
+import {
+  Button,
+  Flex,
+  Heading,
+  Box,
+  Grid,
+  Text,
+  Image,
+  Link,
+} from "@chakra-ui/react";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import CheckoutSteps from "../components/CheckoutSteps";
+
+const PlaceOrderScreen = () => {
+  const cart = useSelector((state) => state.cart);
+  //   console.log(cart);
+  cart.itemsPrice = cart.cartItems.reduce(
+    (acc, curVal) => acc + curVal.price * curVal.qty,
+    0
+  );
+
+  cart.shippingPrice = cart.shippingPrice < 10000 ? 1000 : 0;
+  cart.taxPrice = (18 * cart.itemsPrice) / 100;
+  cart.totalPrice = cart.shippingPrice + cart.taxPrice + cart.itemsPrice;
+
+  const placeOrderHandler = () => {
+    console.log("Placer order");
+  };
+  return (
+    <Flex mt="10" w="full" direction="column" py="5">
+      <CheckoutSteps step1 step2 step3 step4 />
+
+      <Grid templateColumns="3fr 2fr" gap="20" p="5">
+        {/* Column 1 */}
+        <Flex direction="column">
+          <Box borderBottom="2px" py="6" borderColor="gray.400">
+            <Heading as="h2" fontSize="2xl" fontweight="semibold" mb="3">
+              Shipping
+            </Heading>
+            <Text>
+              <strong>Address:</strong> <br />
+              {cart.shippingAddress.address},<br />
+              {cart.shippingAddress.city},{cart.shippingAddress.postalCode},
+              {cart.shippingAddress.country}
+            </Text>
+          </Box>
+
+          <Box borderBottom="2px" py="6" borderColor="gray.400">
+            <Heading as="h2" fontSize="2xl" fontweight="semibold" mb="3">
+              Payment Method
+            </Heading>
+            <Text>
+              <strong>Method:</strong>
+              <span style={{ textTransform: "capitalize" }}>
+                {cart.paymentMethod}
+              </span>
+            </Text>
+          </Box>
+          <Box borderBottom="2px" py="6" borderColor="gray.400">
+            <Heading as="h2" fontSize="2xl" fontweight="semibold" mb="3">
+              Ordered Items
+            </Heading>
+            <Box>
+              {cart.cartItems.length === 0 ? (
+                <Message>Your Cart is Empty</Message>
+              ) : (
+                <Box py="2">
+                  {cart.cartItems.map((item, idx) => (
+                    <Flex
+                      key={idx}
+                      alignItems="center"
+                      justifyContent="space-between"
+                      wrap="wrap"
+                    >
+                      <Flex py="2" alignItems="center">
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          w="150px"
+                          h="150px"
+                          objectFit="cover"
+                          mr="6"
+                        />
+                        <Link
+                          as={RouterLink}
+                          to={`/product/${item.product}`}
+                          fontSize="xl"
+                          fontWeight="medium"
+                        >
+                          {item.name}
+                        </Link>
+                      </Flex>
+                      <Text fontSize="lg" fontWeight="semibold">
+                        {item.qty || 1} X ₹{item.price} = ₹
+                        {(item.qty || 1) * item.price}
+                      </Text>
+                    </Flex>
+                  ))}
+                </Box>
+              )}
+            </Box>
+          </Box>
+        </Flex>
+
+        {/* Column 2 */}
+
+        <Flex
+          direction="column"
+          bgColor="white"
+          justifyContent="space-between"
+          p="8"
+          shadow="md"
+          rounded="lg"
+          borderColor="gray.400"
+        >
+          <Box>
+            <Heading as="h2" fontSize="3xl" fontweight="bold" mb="3">
+              Order Summary
+            </Heading>
+            {/* Number of items */}
+            <Flex
+              my="3"
+              borderBottom="2px"
+              borderColor="gray.400"
+              py="2"
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              <Text fontSize="xl">Number of Items: </Text>
+              <Text fontweight="bold" fontSize="xl">
+                {cart.cartItems.length}
+              </Text>
+            </Flex>
+            {/* Shipping Price */}
+            <Flex
+              my="3"
+              borderBottom="2px"
+              borderColor="gray.400"
+              py="2"
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              <Text fontSize="xl">Shipping </Text>
+              <Text fontweight="bold" fontSize="xl">
+                ₹{cart.shippingPrice}
+              </Text>
+            </Flex>
+            {/* Tax price */}
+
+            <Flex
+              my="3"
+              borderBottom="2px"
+              borderColor="gray.400"
+              py="2"
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              <Text fontSize="xl">Tax </Text>
+              <Text fontweight="bold" fontSize="xl">
+                ₹{cart.taxPrice}
+              </Text>
+            </Flex>
+            {/* Total Price */}
+
+            <Flex
+              my="3"
+              borderBottom="2px"
+              borderColor="gray.400"
+              py="2"
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              <Text fontSize="xl">Total </Text>
+              <Text fontweight="bold" fontSize="xl">
+                ₹{cart.totalPrice}
+              </Text>
+            </Flex>
+          </Box>
+          <Button
+          size="lg"
+          textTransform="uppercase"
+          colorScheme="yellow"
+          type="button"
+          disabled={cart.cartItems === 0}
+          onClick={placeOrderHandler}
+          >Place Order</Button>
+        </Flex>
+      </Grid>
+    </Flex>
+  );
+};
+
+export default PlaceOrderScreen;
