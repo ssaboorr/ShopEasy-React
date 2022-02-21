7eace1873c9544544effe797e1378639497258a7
ssaboorr <48275468+ssaboorr@users.noreply.github.com>
2022-02-21 12:59:13 -0500

    Step 66 completed. Create and Update Product Backend Endpoints done.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -35,3 +35,71 @@ export const deleteProduct = asyncHandler(async (req, res) => {
     throw new Error("Product Not found");
   }
 });
+
+// @desc    Create a product
+// @route   POST /api/products/
+// @access  private/admin
+
+export const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample Product",
+    price: 0,
+    user: req.user._id,
+    image1: "/images/sample.jpg",
+    image2: "/images/sample.jpg",
+    image3: "/images/sample.jpg",
+    image4: "/images/sample.jpg",
+    brand: "sample brand",
+    category: "sample Category",
+    gender: "male",
+    description: "Sample...",
+    numReviews: 0,
+    countInStock: 0,
+    type: "casuals",
+  });
+
+  const createdProduct = await product.save();
+  res.statusMessage(201).json(createdProduct);
+});
+
+// @desc    Update a product
+// @route   PUT /api/products/:id
+// @access  private/admin
+
+export const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image1,
+    image2,
+    image3,
+    image4,
+    brand,
+    category,
+    countInStock,
+    type,
+  } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image1 = image1;
+    product.image2 = image2;
+    product.image3 = image3;
+    product.image4 = image4;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+    product.type = type;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,14 +1,20 @@
 import express from "express";
 import {
+  createProduct,
   deleteProduct,
   getProductById,
   getProducts,
+  updateProduct,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middlewares/authMiddlerware.js";
 const router = express.Router();
 
-router.route("/").get(getProducts);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
 
-router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 export default router;
