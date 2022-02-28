7d0b0d85383a863b18198cee706eb828346e3c62
ssaboorr <48275468+ssaboorr@users.noreply.github.com>
2022-02-28 04:03:29 -0500

    Step 70 completed. Image Upload Configuration and Endpoint

---
+++ backend/routes/uploadsRoutes.js
@@ -0,0 +1,40 @@
+import path from "path";
+import express from "express";
+import multer from "multer";
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}.${path.extname(file.originalname)}`
+    );
+  },
+});
+
+function checkFileTypes(file, cd) {
+  const filetypes = /jpeg|png|gif|jpg/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb("Only iages Allowed");
+  }
+}
+
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    checkFileTypes(file, cb);
+  },
+});
+
+router.post("/", upload.single("image"), (req, res) => {
+  res.send(`/${req.file.path}`);
+});
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import connectDb from "./config/db.js";
+import path from "path";
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
 import { notFound, errorHandler } from "./middlewares/errorMiddlewares.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import path from "path";
 dotenv.config();
 connectDb();
 
@@ -25,6 +27,10 @@ app.use("/api/orders", orderRoutes);
 app.get("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID);
 });
+
+// Create a static folder
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(notFound);
 app.use(errorHandler);
