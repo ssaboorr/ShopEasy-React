73f66df791a13de01ce89c767522dc27de72a93f
saboor-mzaalo <saboor.ansari@mzaalo.com>
2023-06-23 20:24:00 +0530

    created build

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,20 @@
 import mongoose from "mongoose";
 
-
 const connectDb = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
+    console.log("connect database ran");
+    const conn = await mongoose.connect(
+      process.env.MONGO_URI,
+
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      }
+    );
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
+    console.log("catch:::", err);
     console.error(`Error: ${err.message}`);
     process.exit(1);
   }
